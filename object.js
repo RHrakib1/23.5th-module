4cc7c06b254006790e66607978991e2fc4353fd0
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-09 22:27:40 +0600

    add a new object and array

---
+++ object.js
@@ -0,0 +1,24 @@
+// object er akta proparti akta obje o hoite pare array o hoite pare 
+const students={
+    name:'rakib',
+    roll:511100,
+    address:'colony',
+    // array
+    friends:['rifat','raju', 'sahed', 'sumon'],
+    // array er vitor obj 
+    player:[{name:'shakib', age:37}, {name:'masrafi', age:38}],
+    // function 
+    tarin:function(){},
+    // obj....obj er vitor o obj deoya jai 
+    car:{
+        color:'black',
+        price:2544440,
+        name:'BMW',
+        carMake:{
+            name:'markJukarBuk',
+            ceo:'rakib hasan',
+            size:5000
+        }
+    }
+}
+console.log(students.tarin);
